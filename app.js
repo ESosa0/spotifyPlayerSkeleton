efc535b0132b2be92c4940dc0cb663610d2a8d9c
ESosa0 <ericasosa0@gmail.com>
2016-02-12 16:28:27 +0100

    update status bar as song plays

--- app.js
+++ app.js
@@ -8,37 +8,53 @@ $(function() {
   }
 
   SpotifyTrack.prototype.searchTrack = function (e) {
+
     e.preventDefault();
     var keyword = $('.keyword').val();
     $.get('https://api.spotify.com/v1/search?type=track&query=' + keyword, this.handleTrack.bind(this));
   }
 
   SpotifyTrack.prototype.handleTrack = function(response) {
+
     this.name = response.tracks.items[0].name;
     this.artist = response.tracks.items[0].artists[0].name;
     this.imageUrl = response.tracks.items[0].album.images[0].url;
     this.previewUrl = response.tracks.items[0].preview_url;
-    
 
-    this.displayTrack();
-  
+    $('.js-player').attr('src', this.previewUrl)
+    $('.btn-play').removeClass('playing');
     
+    this.displayTrack();  
   };
 
-// When the user clicks the play button, select the <audio> tag with jQuery and trigger playback.
   SpotifyTrack.prototype.playTrack = function(){
-    $('.js-player').attr('src', this.previewUrl)
-    $('.js-player').trigger('play')
+
+    if ($('.btn-play').hasClass('playing')){
+      $('.js-player').trigger('pause');
+
+    } else {
+      $('.js-player').trigger('play');
+    }
+
+      $('.btn-play').toggleClass('playing');
   }
 
   SpotifyTrack.prototype.displayTrack = function(){
+
     $('.title').text(this.name);
     $('.author').text(this.artist);
     $('#image').attr('src', this.imageUrl)
   }
 
+  function printTime () {
+    var current = $('.js-player').prop('currentTime');
+    $('progress').attr('value', current);
+}
+
   var myTrack = new SpotifyTrack();
+
   $('.search').on('click', myTrack.searchTrack.bind(myTrack));
   $('.btn-play').on('click', myTrack.playTrack.bind(myTrack));
+  $('.js-player').on('timeupdate', printTime);
 
 });
